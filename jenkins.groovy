216c2c7a256eec5fd1279b31372e46f360404a4b
limgyeol <limg8299@daum.net>
2026-03-10 14:53:45 +0900

    feat: Add Sample

--- jenkins.groovy
+++ jenkins.groovy
@@ -6,7 +6,7 @@ pipeline {
         AWS_ACCOUNT_ID = "637423625226"
         AWS_REGION     = "ap-northeast-2" 
         ECR_REGISTRY   = "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
-        REPO_NAME      = "demo-ecr"
+        ECR_NAME      = "demo-ecr"
         IMAGE_TAG      = "${env.BUILD_NUMBER}" // 빌드 번호를 태그로 사용
     }
 
@@ -20,10 +20,9 @@ pipeline {
         stage('Docker Build') {
             steps {
                 script {
-                    sh "docker build -t ${REPO_NAME}:${IMAGE_TAG} ."
-                    // ECR용 풀 네임으로 태깅
-                    sh "docker tag ${REPO_NAME}:${IMAGE_TAG} ${ECR_REGISTRY}/${REPO_NAME}:${IMAGE_TAG}"
-                    sh "docker tag ${REPO_NAME}:${IMAGE_TAG} ${ECR_REGISTRY}/${REPO_NAME}:latest"
+                    sh "docker build -t ${ECR_NAME}:${IMAGE_TAG} ."
+                    sh "docker tag ${ECR_NAME}:${IMAGE_TAG} ${ECR_REGISTRY}/${ECR_NAME}:${IMAGE_TAG}"
+                    sh "docker tag ${ECR_NAME}:${IMAGE_TAG} ${ECR_REGISTRY}/${ECR_NAME}:latest"
                 }
             }
         }
@@ -33,16 +32,16 @@ pipeline {
                 script {
                     sh "aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${ECR_REGISTRY}"
                     
-                    sh "docker push ${ECR_REGISTRY}/${REPO_NAME}:${IMAGE_TAG}"
-                    sh "docker push ${ECR_REGISTRY}/${REPO_NAME}:latest"
+                    sh "docker push ${ECR_REGISTRY}/${ECR_NAME}:${IMAGE_TAG}"
+                    sh "docker push ${ECR_REGISTRY}/${ECR_NAME}:latest"
                 }
             }
         }
 
         stage('Cleanup') {
             steps {
-                sh "docker rmi ${ECR_REGISTRY}/${REPO_NAME}:${IMAGE_TAG}"
-                sh "docker rmi ${ECR_REGISTRY}/${REPO_NAME}:latest"
+                sh "docker rmi ${ECR_REGISTRY}/${ECR_NAME}:${IMAGE_TAG}"
+                sh "docker rmi ${ECR_REGISTRY}/${ECR_NAME}:latest"
             }
         }
     }
